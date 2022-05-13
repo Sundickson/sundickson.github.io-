87fa5955def512dece132e44735dbcc3f82e45bc
Sundickson <98136130+Sundickson@users.noreply.github.com>
2022-05-13 17:30:07 +0100

    Add files via upload
    my portfolio

---
+++ Portfolio 2/script.js
@@ -0,0 +1,23 @@
+$('#menu-icon').click(function(){
+        $('#menu-icon').toggleClass("active");
+        $('.navlist').toggleClass("active");
+    });
+
+
+
+
+
+
+/*
+let menu = document.queryselector("#menu-icon");
+let navlist = document.queryselector(".navlist");
+
+menu.addEventListener("click", function () {
+	navlist.classList.toggle("active");
+});
+
+window.onscroll = () => {
+	navlist.classList.remove("active");
+};   
+
+
