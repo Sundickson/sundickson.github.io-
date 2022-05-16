417060d9a0900c9e690d418fa888526a1fdfc716
Victor Eke <evavic44@gmail.com>
2022-05-16 10:33:32 +0100

    :zap: Remove unused script code

--- Portfolio 2/script.js
+++ Portfolio 2/script.js
@@ -1,20 +1,3 @@
-// $('#menu-icon').click(function(){
-//         $('#menu-icon').toggleClass("active");
-//         $('.navlist').toggleClass("active");
-//     });
-
-// /*
-// let menu = document.queryselector("#menu-icon");
-// let navlist = document.queryselector(".navlist");
-
-// menu.addEventListener("click", function () {
-// 	navlist.classList.toggle("active");
-// });
-
-// window.onscroll = () => {
-// 	navlist.classList.remove("active");
-// };
-
 let menuIcon = document.querySelector(".menu-icon");
 let navlist = document.querySelector(".navlist");
 
